50d05222163b9045b97e541e042bb2db2d846ba7
gorkem <gorkemyavas98@gmail.com>
2022-03-17 10:13:33 +0300

    I Lov Xamarin !!!

--- Week 3/Week 3/MainPage.xaml.cs
+++ Week 3/Week 3/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Week_3
 {
@@ -12,6 +6,47 @@ namespace Week_3
     {
         public MainPage()
         {
+
+            var formattedString = new FormattedString();
+
+            formattedString.Spans.Add(
+                new Span()
+                {
+                    Text = "\tI\t",
+
+                });
+            formattedString.Spans.Add(
+                new Span()
+                {
+                    Text = "\tLove\t",
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                    FontAttributes = FontAttributes.Bold
+
+                });
+            formattedString.Spans.Add(
+                new Span()
+                {
+                    Text = "\tXamarin.Forms!!!\t",
+                    //FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                    BackgroundColor = Color.Tomato,
+                    ForegroundColor = Color.White,
+                    FontAttributes = FontAttributes.Italic
+
+                });
+
+            Content = new Label
+            {
+                FormattedText = formattedString,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            }; ;
+
+
+
+
+
+
             InitializeComponent();
         }
     }
